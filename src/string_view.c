a6de7aa2a23bfec6182e39065121e07e0fb98ce2
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-09-15 20:31:58 +0300

    string_view_equal

--- src/string_view.c
+++ src/string_view.c
@@ -2,9 +2,23 @@
 #include <string.h>
 
 StringView string_view_from_cstr(const char *data) {
-    size_t length = nullptr != data ? strlen(data) : 0;
+    size_t     length = nullptr != data ? strlen(data) : 0;
 
     StringView view = {.data = (char *)data, length};
 
     return view;
+}
+
+bool string_view_equal(StringView a, StringView b) {
+    if (a.length != b.length) {
+        return false;
+    }
+
+    for (size_t i = 0; i < a.length; i++) {
+        if (a.data[i] != b.data[i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
--- tests/string_view.test.c
+++ tests/string_view.test.c
@@ -32,3 +32,29 @@ TEST(from_cstr_with_nullptr, {
         test_assert(view.data[i] == data[i]);
     }
 })
+
+typedef struct {
+    const char *a;
+    const char *b;
+    const bool expected;
+} EqualData;
+TEST_DATA_PROVIDER(equalDataProvider, {
+    const static EqualData item1 = { "abc", "abc", true };
+    test_data_add("abc abc", item1);
+
+    const static EqualData item2 = { "abc", "cba", false };
+    test_data_add("abc cba", item2);
+
+    const static EqualData item3 = { "", "", false };
+    test_data_add(" ", item3);
+})
+
+TEST(equal, {
+    const EqualData *data = test_data_get(EqualData);
+    const StringView a = string_view_from_cstr(data->a);
+    const StringView b = string_view_from_cstr(data->b);
+    const bool expected = data->expected;
+
+    test_assert(string_view_equal(a, b) == expected);
+})
+TEST_OPTIONS(equal, .dataProvider = equalDataProvider())
