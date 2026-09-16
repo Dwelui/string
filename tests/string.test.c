3ec384dd3702d27f772c4e01dadc8bac5f8406cf
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-09-16 21:28:25 +0300

    clone test

--- tests/string.test.c
+++ tests/string.test.c
@@ -40,21 +40,22 @@ TEST(from_cstr, {
 })
 TEST_OPTIONS(from_cstr, .dataProvider = stringDataProvider())
 
-// TEST(clone, {
-//     const char *data   = test_data_get(char);
-//     size_t      length = strlen(data);
-//
-//     String      string = string_from_data(data, length);
-//     String      clone  = string_clone(string);
-//
-//     test_assert(string.length == length);
-//
-//     for (size_t i = 0; i < string.length; i++) {
-//         test_assert(string.data[i] == data[i]);
-//     }
-//
-//     string_destroy(&string);
-// })
+TEST(clone, {
+    const char *data = test_data_get(char);
+
+    String      string = string_from_cstr(data);
+    String      clone  = string_clone(string);
+    string_destroy(&string);
+
+    test_assert(clone.length == strlen(data));
+
+    for (size_t i = 0; i < clone.length; i++) {
+        test_assert(clone.data[i] == data[i]);
+    }
+
+    string_destroy(&clone);
+})
+TEST_OPTIONS(clone, .dataProvider = stringDataProvider())
 
 TEST(from_data_outlives_original_heap_data, {
     const char *data   = test_data_get(char);
