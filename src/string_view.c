72b6e6d8cdc933c7f18ef83affec15a13f1eac06
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-09-17 21:14:50 +0300

    string_view_from_string

--- src/string_view.c
+++ src/string_view.c
@@ -1,6 +1,10 @@
 #include <dwelui/string.h>
 #include <string.h>
 
+StringView string_view_from_string(String s) {
+    return (StringView){s.data, s.length};
+}
+
 StringView string_view_from_cstr(const char *data) {
     size_t     length = nullptr != data ? strlen(data) : 0;
 
--- tests/string_view.test.c
+++ tests/string_view.test.c
@@ -4,13 +4,28 @@
 #include <string.h>
 #include "data_providers.c"
 
+TEST(from_string, {
+    const char *data = test_data_get(char);
+
+    String      string = string_from_cstr(data);
+    StringView  view   = string_view_from_string(string);
+
+    test_assert(view.length == strlen(data));
+
+    for (size_t i = 0; i < view.length; i++) {
+        test_assert(view.data[i] == data[i]);
+    }
+
+    string_destroy(&string);
+})
+TEST_OPTIONS(from_string, .dataProvider = cstrDataProvider())
+
 TEST(from_cstr, {
-    const char *data   = test_data_get(char);
-    size_t      length = strlen(data);
+    const char *data = test_data_get(char);
 
     StringView  view = string_view_from_cstr(data);
 
-    test_assert(view.length == length);
+    test_assert(view.length == strlen(data));
 
     for (size_t i = 0; i < view.length; i++) {
         test_assert(view.data[i] == data[i]);
